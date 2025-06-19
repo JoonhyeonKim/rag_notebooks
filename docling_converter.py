e5387bbe9113c06d617a93fa772f6a7699425303
김준현 <joonhyeonkim1015@gmail.com>
2025-06-19 17:12:33 +0900

    docling converter version pcm

--- docling_converter.py
+++ docling_converter.py
@@ -2,3 +2,11 @@ from langchain_docling import DoclingLoader
 import pathlib
 pdf_path = pathlib.Path("../../books/PCM.pdf")
 print(pdf_path)
+
+loader = DoclingLoader(file_path=pdf_path)
+# docs = loader.load()
+# print(docs)
+
+doc_iter = loader.lazy_load()
+for doc in doc_iter:
+    print(doc)
